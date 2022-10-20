90f3b151a54c4aaae939a195b14f37b5582a40f8
jewel109 <jewelrana176517@gmail.com>
2022-10-20 07:29:54 +0600

    lc-35

---
+++ sliding-window/lc-35/1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+
+int main(){
+	string s;
+	cin >> s;
+	int n = s.size();
+	int left = 0;
+	int right = 0;
+	int length = 0;
+	vector<int> map(256, -1);
+	while(right < n){
+		cout << "right: " << right << endl;
+		if(map[s[right]] != -1) left= max(left, map[s[right]]+1);		
+		cout << "left : " << left << endl;
+		map[s[right]] = right;
+		cout << "key: " << s[right] << " val : "<< right<< endl;
+		length = max(length, right -left + 1);
+		cout << "length: " << length << endl;
+		right++;
+		cout << endl;
+	}
+	cout<< "length : " << length << endl;
+}
