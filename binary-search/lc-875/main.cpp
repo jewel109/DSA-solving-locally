cac50173fed98fd252355cea3b31046641d14f3c
jewel109 <jewelrana176517@gmail.com>
2022-11-02 08:51:51 +0600

    all ds list updated

---
+++ binary-search/lc-875/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+
+int main() {
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  int s;
+
+
+
+  // vector<int>v{1,2,3,4,5};
+  //
+  cout << ceil(1.0 * 55555 / 4) << endl;
+  // for (int i=0; i < v.size(); i++) {
+  //   cout << "vector : at " << i << " "<< v[i] << endl;
+  // }
+
+}
