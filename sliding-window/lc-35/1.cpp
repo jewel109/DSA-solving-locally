15c33577cf5abfc285ba797640625b07904ce641
jewel109 <jewelrana176517@gmail.com>
2022-11-14 16:48:57 +0600

    initial commit

--- sliding-window/lc-35/1.cpp
+++ sliding-window/lc-35/1.cpp
@@ -25,4 +25,16 @@ int main(){
 		cout << endl;
 	}
 	cout<< "length : " << length << endl;
+{
+#ifndef ONLINE_JUDGE
+ 
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+ 
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+ 
+#endif
+    return 0;
+
 }
---
+++ sliding-window/lc-567/1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a = 'z' - 'a';
+	int k = 'z';
+	int l = 'a' - k;
+	cout << l << endl;
+
+	cout << k << endl;
+
+	cout << a << endl;
+}
